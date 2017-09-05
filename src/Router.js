49350a206bc7a5261a58fc764276043eebd828d7
Antione Mace <amace@sargent-disc.com>
2017-09-05 17:44:08 +0100

    PropType fix

--- src/Router.js
+++ src/Router.js
@@ -7,9 +7,8 @@
  *
  */
 import React, {
-  Component,
-  PropTypes,
-} from 'react';
+  Component } from 'react';
+import PropTypes from 'prop-types';
 import { BackHandler } from 'react-native';
 import NavigationExperimental from 'react-native-experimental-navigation';
 
