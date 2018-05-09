5a79fbac409341a892355c068335400a083c7060
Antione Mace <amace@sargent-disc.com>
2018-05-09 16:26:52 +0100

    v3

---
+++ Example/components/TabIcon.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Text,
+} from 'react-native';
+
+const propTypes = {
+  selected: PropTypes.bool,
+  title: PropTypes.string,
+};
+
+const TabIcon = (props) => (
+  <Text
+    style={{ color: props.selected ? 'red' : 'black' }}
+  >
+    {props.title}
+  </Text>
+);
+
+TabIcon.propTypes = propTypes;
+
+export default TabIcon;
